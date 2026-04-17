712abd00f6e51444fc916e434bc3282a79fdcbbe
kucheryaska <kucheryaska12@gmail.com>
2026-04-17 16:15:36 +0300

    JS_2.1_v2

--- task.js
+++ task.js
@@ -3,15 +3,17 @@ const dropdown__value = document.querySelector(".dropdown__value");
 const dropdown__list = document.querySelector(".dropdown__list");
 
 
-button.addEventListener("click", function() {
-    dropdown__list.classList.add("dropdown__list_active");
-});
+const toggleDropdownList = function() {
+    dropdown__list.classList.toggle("dropdown__list_active");
+}
+
+button.addEventListener("click", toggleDropdownList);
 
 const buttonsList = document.querySelectorAll(".dropdown__item");
 buttonsList.forEach((A) => {
     A.addEventListener("click", (event) => {
         event.preventDefault();
-        dropdown__list.classList.remove("dropdown__list_active");
+        toggleDropdownList();
         dropdown__value.textContent = A.textContent;
     });
 })    
